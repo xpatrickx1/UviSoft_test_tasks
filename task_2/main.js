90d5ad165ed01e94900db40d7fbd7c958c61b014
 Patrick <patrick@MacBook-Pro.local>
2020-11-10 10:46:02 +0200

    modified

--- task_2/main.js
+++ task_2/main.js
@@ -1,34 +1,31 @@
 $( document ).ready( function () {
-    $( 'tr' ).click( function ( e ) {
-        if ( e.target.tagName !== 'TH' ) {
-            $( '.user' ).removeClass( 'active' );
-            $( '.name__header' ).addClass( 'active' ).attr( 'colspan', '3' );
-            $( '.phone__header, .email, .user__skype, .user__comment' ).addClass( 'hide' );
-            $( '.details' ).addClass( 'cell_show' ).attr( 'rowspan', '0' );
-            $( '.details__header' ).addClass( 'cell_show' );
-            $( this.closest( '.user' ) ).addClass( 'active' );
-            $( '.user > .user__cell' ).addClass( 'open__td' );
-            $( '.user' ).addClass( 'open__tr' );
-            $('.close').css('display', 'block');
-            $.getJSON( 'data.json', ( data ) => {
-                data.forEach( ( i ) => {
-                    if ( i.name == $( this ).find( '.user__name' ).html() ) {
-                        let details = []
-                        for ( let [ key, value ] of Object.entries( i.details ) ) {
-                            details.push(`${key}: ${value}`)
-                        }
-                        $( '.details' ).html( details.join('<br>') )
+    $( '.user' ).click( function () {
+        $( '.user' ).removeClass( 'active' );
+        $( '.name__header, .users__table, .details__table' ).addClass( 'active' );
+        $( '.phone__header, .email, .user__skype, .user__comment' ).addClass( 'hide' );
+        $( this.closest( '.user' ) ).addClass( 'active' );
+        $( '.user > .user__cell' ).addClass( 'open__td' );
+        $( '.user, .row__header' ).addClass( 'open__tr' );
+        $( '.close' ).css( 'display', 'block' );
+        $.getJSON( 'data.json', ( data ) => {
+            data.forEach( ( i ) => {
+                if ( i.name == $( this ).find( '.user__name' ).html() ) {
+                    let details = [];
+                    for ( let [ key, value ] of Object.entries( i.details ) ) {
+                        details.push( `${key}: ${value}` )
                     }
-                } )
+                    $( '.details' ).html( details.join( '<br>' ) )
+                }
             } )
-        }
+        } )
+
     } );
-    $('.close').click(function(){
-        $('.close').css('display', 'none');
-        $( '.user' ).removeClass( 'active open__tr' );
-        $( '.name__header' ).removeClass( 'active' ).attr( 'colspan', '1' );
+    $( '.close' ).click( function () {
+        $( '.close' ).css( 'display', 'none' );
+        $( '.user, .row__header' ).removeClass( 'active open__tr' );
+        $( '.name__header, .users__table, .details__table' ).removeClass( 'active' );
         $( '.phone__header, .email, .user__skype, .user__comment' ).removeClass( 'hide' ).addClass( 'show' );
         $( '.details, .details__header' ).removeClass( 'cell_show' );
         $( '.user > .user__cell' ).removeClass( 'open__td' );
-    });
+    } );
 } );
